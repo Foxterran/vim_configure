1bdd0c2cfe3b431c18de8ce241658dc29872aa23
cse <cse@cse-lab.localdomain>
2014-07-16 22:15:47 -0700

    update install.sh to sudo

--- install.sh
+++ install.sh
@@ -5,29 +5,29 @@ vimbundle=~/.vim/bundle
 
 #step 1 init vim runtime
 touch ~/.vimrc
-mkdir "$vimruntime"
-mkdir "$vimautoload"
-mkdir "$vimbundle"
+sudo mkdir "$vimruntime"
+sudo mkdir "$vimautoload"
+sudo mkdir "$vimbundle"
 
 echo "step 1 start!"
 #step 2 copy colors theme file 
 echo "copy colors"
-cp -r ./colors "$vimruntime"/
+sudo cp -r ./colors "$vimruntime"/
 
 #step 3 copy the vimrc file
 echo "copy vimrc file"
-cp ./vimrc ~/.vimrc
+sudo cp ./vimrc ~/.vimrc
 
 #step 4 install pathogen
 echo "step 4 start!"
-cp ./pathogen.vim "$vimautoload"/
+sudo cp ./pathogen.vim "$vimautoload"/
 
 #step 5 install nerdcommenter
 echo "step 5 start!"
-cp -r ./nerdcommenter/plugin "$vimruntime"/
-cp -r ./nerdcommenter/doc "$vimruntime"/
+sudo cp -r ./nerdcommenter/plugin "$vimruntime"/
+sudo cp -r ./nerdcommenter/doc "$vimruntime"/
 
 #step 6 install nerdtree by pathogen
 echo "step 6 start!"
-cp -r ./nerdtree "$vimbundle"/
+sudo cp -r ./nerdtree "$vimbundle"/
 
